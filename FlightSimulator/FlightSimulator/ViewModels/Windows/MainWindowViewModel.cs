b23456d41af088fbf7c9a7883661279c1f6f8191
Elad Bitton <eladbitton1@gmail.com>
2019-04-06 10:25:19 +0300

    UI

---
+++ FlightSimulator/FlightSimulator/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using FlightSimulator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FlightSimulator.Views.Windows;
+
+namespace FlightSimulator.ViewModels.Windows
+{
+    class MainWindowViewModel : BaseNotify
+    {
+        #region OpenSettingsCommand
+        private ICommand _settingsCommand;
+        public ICommand OpenSettingsCommand
+        {
+            get
+            {
+                return _settingsCommand ?? (_settingsCommand = new CommandHandler(() => OnOpenSettings()));
+            }
+        }
+
+        private void OnOpenSettings()
+        {
+            Settings s = new Settings();
+            s.Show();
+        }
+        #endregion
+    }
+}
---
+++ FlightSimulator/FlightSimulator/ViewModels/Windows/SettingsWindowViewModel.cs
@@ -0,0 +1,100 @@
+﻿using FlightSimulator.Model;
+using FlightSimulator.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FlightSimulator.ViewModels.Windows
+{
+    public class SettingsWindowViewModel : BaseNotify
+    {
+        private ISettingsModel model;
+        public Action CloseAction { get; set; }
+
+        public SettingsWindowViewModel()
+        {
+            this.model = ApplicationSettingsModel.Instance;
+        }
+
+        public string FlightServerIP
+        {
+            get { return model.FlightServerIP; }
+            set
+            {
+                model.FlightServerIP = value;
+                NotifyPropertyChanged("FlightServerIP");
+            }
+        }
+
+        public int FlightCommandPort
+        {
+            get { return model.FlightCommandPort; }
+            set
+            {
+                model.FlightCommandPort = value;
+                NotifyPropertyChanged("FlightCommandPort");
+            }
+        }
+
+        public int FlightInfoPort
+        {
+            get { return model.FlightInfoPort; }
+            set
+            {
+                model.FlightInfoPort = value;
+                NotifyPropertyChanged("FlightInfoPort");
+            }
+        }
+
+     
+
+        public void SaveSettings()
+        {
+            model.SaveSettings();
+        }
+
+        public void ReloadSettings()
+        {
+            model.ReloadSettings();
+        }
+
+        #region Commands
+        #region SaveCommand
+        private ICommand _saveCommand;
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return _saveCommand ?? (_saveCommand = new CommandHandler(() => OnSave()));
+            }
+        }
+        private void OnSave()
+        {
+            model.SaveSettings();
+            CloseAction();
+        }
+        #endregion
+
+        #region CancelCommand
+        private ICommand _cancelCommand;
+        public ICommand CancelCommand
+        {
+            get
+            {
+                return _cancelCommand ?? (_cancelCommand = new CommandHandler(() => OnCancel()));
+            }
+        }
+        private void OnCancel()
+        {
+            model.ReloadSettings();
+            CloseAction();
+        }
+        #endregion
+        #endregion
+    }
+}
+
